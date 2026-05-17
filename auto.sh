2800e573b55869e7a79535d123e02122da86f353
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-17 14:11:55 -0300

    added coments

--- auto.sh
+++ auto.sh
@@ -20,12 +20,14 @@ sudo python3 /tmp/install.py
 # Validação 0000 do AIED #
 ##########################
 
+echo "Teste de ambiente"
 sudo aied validar 0000 ola > TesteAIED.txt
 
 #################################
 # Validação Prática 98a917ce 01 #
 #################################
 
+echo "3.6.1 Criando o ambiente da aula"
 sudo aied validar 98a917ce checkpoint01 > 98a917ce01.txt
 
 
