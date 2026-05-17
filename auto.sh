375efe54dcaf31d147e860efecd8b907ca428971
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-17 13:58:12 -0300

    primeira atividade

---
+++ auto.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+######################
+# Instalação do Sudo #
+######################
+su root
+apt update -y
+apt install sudo -y
+echo "userlinux ALL=(ALL:ALL) ALL" >> /etc/sudoers
+exit
+
+######################
+# Instalação do AIED #
+######################
+wget -O /tmp/install.py http://www.aied.com.br/linux/download/install_v2.py
+sudo apt install libjsoncpp-dev -y
+sudo python3 /tmp/install.py
+
+##########################
+# Validação 0000 do AIED #
+##########################
+
+sudo aied validar 0000 ola > TesteAIED.txt
+
+#################################
+# Validação Prática 98a917ce 01 #
+#################################
+
+sudo aied validar 98a917ce checkpoint01 > 98a917ce01.txt
+
+
