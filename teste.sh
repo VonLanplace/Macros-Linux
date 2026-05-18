3960bebc4cd77ba5363719cf722b10bb8fd989a3
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-18 15:45:53 -0300

    10

--- teste.sh
+++ teste.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # 1. Pergunta o nome da interface de forma correta e guarda na variável 'interface'
+ip a
 read -p "Digite o nome da interface (ex: enp0s3): " interface
 
 # 2. Escreve no arquivo (repare que tiramos as aspas do EOF para permitir o uso da variável)
@@ -18,6 +19,7 @@ iface enp1s0 inet static
     netmask 255.255.255.0
     gateway 10.0.2.2
     dns-nameservers 8.8.8.8
+    broadcast 10.0.2.255
 EOF
 
 # 3. Reinicia a rede para aplicar a alteração antes de validar
