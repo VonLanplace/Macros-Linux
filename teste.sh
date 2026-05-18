ac1c813da0f377847feec98600853ee90599e6c2
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-18 15:08:19 -0300

    teste 9.1

--- teste.sh
+++ teste.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-ps aux | grep python > ~/typescript
-sudo aied validar prc0001 checkpoint01
+sudo cat <<'EOF' > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+auto eth0
+iface eth0 inet static
+    address 10.0.2.3
+    netmask 255.255.255.0
+    gateway 10.0.2.2
+    dns-nameservers 8.8.8.8
+EOF
+sudo aied validar 0002 checkpoint03 
