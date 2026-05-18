08051022c93e70b2cc53af30aec7a440f2ce2881
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-18 15:39:49 -0300

    6

--- teste.sh
+++ teste.sh
@@ -4,26 +4,25 @@
 read -p "Digite o nome da interface (ex: enp0s3): " interface
 
 # 2. Escreve no arquivo (repare que tiramos as aspas do EOF para permitir o uso da variável)
-sudo tee /etc/network/interfaces << EOF
+sudo tee /etc/network/interfaces << 'EOF'
 source /etc/network/interfaces.d/*
 
 # Interface Loopback
 auto lo
 iface lo inet loopback
 
-# Interface de Rede Estática
-auto $interface
-iface $interface inet static
+# Interface de Rede Estática Correta
+auto enp1s0
+iface enp1s0 inet static
     address 10.0.2.3
     netmask 255.255.255.0
     gateway 10.0.2.2
     dns-nameservers 8.8.8.8
-    broadcast 10.0.2.255
 EOF
 
 # 3. Reinicia a rede para aplicar a alteração antes de validar
 echo "Reiniciando o serviço de rede..."
-sudo systemctl restart networking
+#sudo systemctl restart networking
 
 # 4. Executa a validação do seu checkpoint
 sudo aied validar 0002 checkpoint03
