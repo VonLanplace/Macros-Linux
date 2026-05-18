15aa5dcc7e0814242860e8e730dacab6535d0935
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-18 14:00:48 -0300

    fix para teste

--- teste.sh
+++ teste.sh
@@ -10,12 +10,12 @@ read -p "Digite o nome do disco que deseja formatar (ex: vdb): " DISK_INPUT
 DISK_INPUT=$(echo "$DISK_INPUT" | sed 's|^/dev/||')
 
 DISK="/dev/$DISK_INPUT"
-PARTICAO="${DISK}1"
+PARTITION="${DISK}1"
 
 echo -e "g\nn\n1\n\n\nw" | sudo fdisk $DISK
-sudo mkfs.ext4 $DISK
+sudo mkfs.ext4 $PARTITION
 sudo mkdir -p /backup
-echo "UUID=$(blkid -s UUID -o value $DISK) /backup ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "UUID=$(sudo lsblk -no UUID $PARTITION) /backup ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
 sudo mount -a
 
 sudo aied validate 8b65b431 checkpoint01 | tee ~/6.15.1.txt
