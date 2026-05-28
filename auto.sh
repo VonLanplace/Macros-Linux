efd32081b05c36ab11fb404c1bf57ffd3533c555
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-28 09:08:13 -0300

    terminado 14

--- auto.sh
+++ auto.sh
@@ -581,3 +581,35 @@ wait
 
 sudo aied validar py0001 checkpoint05 | tee ~/12.6.5
 
+##########
+# 14.4.1 #
+##########
+sudo apt install at -y
+
+echo "touch /tmp/teste.txt" | at 12:00
+wait
+
+sudo aied validar 4d4f6ae checkpoint01 | tee ~/14.4.1
+##########
+# 14.4.2 #
+##########
+
+tee ~/script.sh << 'EOF'
+#!/bin/bash
+#nome: script.sh
+#diretório: ~/
+#descrição: Faz o backup do diretorio do usuário, é apenas um exemplo de aula
+
+data=$(/bin/date +%d-%m-%Y);
+
+#atenção, vou guardar em /tmp para
+tar -cvzf /tmp/backup-${LOGNAME}-${data}.tar.gz /home/${LOGNAME}/;
+EOF
+
+chmod +x ~/script.sh
+
+(crontab -l 2>/dev/null; echo "0 12 * * * /bin/bash ~/script.sh") | crontab -
+wait
+
+sudo aied validar d7a527b checkpoint01 | tee ~/14.4.2
+
