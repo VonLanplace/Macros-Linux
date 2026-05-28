ff374adfd9d3562a4d3740fc17e8c86377e94e2a
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-28 09:14:27 -0300

    teste 15-1

--- teste.sh
+++ teste.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+##########
+# 15.5.1 #
+##########
+
+sudo apt update
+sudo apt install net-tools -y
+sudo apt install openssh-server -y
+
+sudo aied validar 57671beb checkpoint01 | tee ~/15.5.1
